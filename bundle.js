edb6f9150e4e5914585a747ae3f5cd2be972f36e
Archimedes Li <escaperecovery@gmail.com>
2023-08-08 15:09:51 -0400

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,1 @@
+(()=>{var r={10:r=>{r.exports={hello:()=>console.log("WORLD YAYA PLEASE WORK"),POOP:4}}},o={};!function e(t){var s=o[t];if(void 0!==s)return s.exports;var n=o[t]={exports:{}};return r[t](n,n.exports,e),n.exports}(10)})();
